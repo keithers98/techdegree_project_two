d03dd686026d118f88248e1c2c14284405681b29
Keith Frazier <keithfrazier98@gmail.com>
2020-07-22 15:18:29 -0700

    Added balance_teams
    The balance teams function is defined rather weird, I think it needs a loop instead of how I defined it to be able to achieve some of the extra tasks. It all works though so I am moving to finishing th eidplsy stats funcitonality, then will revise everything to implement the extra tasks.

--- app.py
+++ app.py
@@ -1,27 +1,42 @@
 import constants
 import copy
-print("""
-Basketball Team Stats Tool \n
-----MENU---- \n
-Here are your choices:
-1) Display Team Stats
-2) Quit \n
-""")
 
-menu_option = input("Enter an option >>>   ")
-players_copy = copy.deepcopy(constants.PLAYERS) #every shallow copy will change all the existing dictionaries
+players = copy.deepcopy(constants.PLAYERS) #every shallow copy will change all the existing dictionaries
 
-def clean_data(module):
-    for dicti in players_copy:
-        if dicti['experience'] == 'YES':
-            players_copy[players_copy.index(dicti)]['experience'] = True
-            #dict['experience'] = True
-        elif dicti['experience'] == 'NO':
-            players_copy[players_copy.index(dicti)]['experience'] = False
-            #dict['experience'] = False
+teams = [{constants.TEAMS[0]:[]}, {constants.TEAMS[1]:[]}, {constants.TEAMS[2]:[]}]
 
-        dicti['height'] = int(dicti['height'][0:2])
-    return players_copy
+def clean_data(player_stats):
+    for player in player_stats:
+        if player['experience'] == 'YES':
+            #player_stats[player_stats.index(player)]['experience'] = True #same result as line above but way too complicated!
+            player['experience'] = True
+        elif player['experience'] == 'NO':
+            #player_stats[player_stats.index(player)]['experience'] = False #same result as line above but way too complicated!
+            player['experience'] = False
+
+        player['height'] = int(player['height'][0:2])
+    return player_stats
+
+def balance_teams(player_stats, teams):
+    players_per_team = int(len(player_stats) / len(teams))
+    teams[0][constants.TEAMS[0]].append(player_stats[0:players_per_team])
+    teams[1][constants.TEAMS[1]].append(player_stats[len(teams[0][constants.TEAMS[0]][0]):(players_per_team * 2)])
+    teams[2][constants.TEAMS[2]].append(player_stats[len(teams[1][constants.TEAMS[1]][0]) * 2:(players_per_team * 3)])
+
+
+#def display_stats(yes_no):
+
+
 
 if __name__ == '__main__':
-    clean_data(players_copy)
+    print("""
+    Basketball Team Stats Tool \n
+         ----MENU---- \n
+    Here are your choices:
+    1) Display Team Stats
+    2) Quit \n
+    """)
+
+    menu_option = input("Enter an option >>>   ")
+    clean_data(players)
+    balance_teams(players, teams)
