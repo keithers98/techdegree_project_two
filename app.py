478d068d50ebfe0be39352152946575f13b38c5c
Keith Frazier <keithfrazier98@gmail.com>
2020-07-22 15:43:04 -0700

    Added display_stats
    The display stats function is in a basic form but currently displays the stats from the balance_teams function correctly. It needs to filter the data better so it is more readable when printed.

--- app.py
+++ app.py
@@ -17,18 +17,15 @@ def clean_data(player_stats):
         player['height'] = int(player['height'][0:2])
     return player_stats
 
+
 def balance_teams(player_stats, teams):
     players_per_team = int(len(player_stats) / len(teams))
     teams[0][constants.TEAMS[0]].append(player_stats[0:players_per_team])
     teams[1][constants.TEAMS[1]].append(player_stats[len(teams[0][constants.TEAMS[0]][0]):(players_per_team * 2)])
     teams[2][constants.TEAMS[2]].append(player_stats[len(teams[1][constants.TEAMS[1]][0]) * 2:(players_per_team * 3)])
+    return teams[0]['Panthers'], teams[1]['Bandits'], teams[2]['Warriors']
 
-
-#def display_stats(yes_no):
-
-
-
-if __name__ == '__main__':
+def display_stats(team1, team2, team3):
     print("""
     Basketball Team Stats Tool \n
          ----MENU---- \n
@@ -38,5 +35,24 @@ if __name__ == '__main__':
     """)
 
     menu_option = input("Enter an option >>>   ")
-    clean_data(players)
-    balance_teams(players, teams)
+
+    while menu_option != 0:
+        print('''
+
+        1)Panthers \n
+        2)Bandits \n
+        3)Warriors\n\n''')
+        team_choice = input('Enter an option>>>   ')
+        if team_choice == '1':
+            print(team1)
+        elif team_choice == '2':
+            print(team2)
+        else:
+            print(team3)
+
+
+
+if __name__ == '__main__':
+    player_stats = clean_data(players)
+    panthers, bandits, warriors = balance_teams(players, teams)
+    display_stats(panthers, bandits, warriors)
