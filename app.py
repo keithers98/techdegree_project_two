b7edf56357c259cc3d3c7e5240c5a489f0c7bfa9
Keith Frazier <keithfrazier98@gmail.com>
2020-07-23 00:23:50 -0700

    Cleaned up and added readability
    Added extra lines for readability. Realized instructions say to have the main menu repromt so Im about to redo the continue or quit functionality.

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import constants
 import copy
 
 players = copy.deepcopy(constants.PLAYERS) #every shallow copy will change all the existing dictionaries
-
+team_names = constants.TEAMS
 teams = [{constants.TEAMS[0]:[]}, {constants.TEAMS[1]:[]}, {constants.TEAMS[2]:[]}]
 
 def clean_data(player_stats):
@@ -25,26 +25,26 @@ def balance_teams(player_stats, teams):
     teams[2][constants.TEAMS[2]].append(player_stats[len(teams[1][constants.TEAMS[1]][0]) * 2:(players_per_team * 3)])
     return teams[0]['Panthers'], teams[1]['Bandits'], teams[2]['Warriors']
 
-def display_stats(team1, team2, team3):
-    print("""
+def display_stats(team1, team2, team3, team_names):
+    main_menu = ("""
     Basketball Team Stats Tool \n
          ----MENU---- \n
     Here are your choices:
     1) Display Team Stats
     2) Quit \n
     """)
-
-    menu_option = input("Enter an option >>>   ")
+    print(main_menu)
+    menu_option = input("\nEnter an option >>>   ")
 
     def ask_again():
-        menu_option = input("Would you like to (C)ontinue or (Q)uit? >>   ")
+        menu_option = input("\nWould you like to (C)ontinue or (Q)uit? >>   ")
         try:
             if menu_option == 'c':
                 menu_option = '1'
             elif menu_option == 'q':
                 menu_option = '2'
             elif menu_option != 'c' or 'q':
-                raise ValueError("\n\nWhoops, enter 'c' to continue or 'q' to quit.\n\n")
+                raise ValueError("\n\nWhoops, enter 'c' to continue or 'q' to quit.\n")
         except ValueError as err:
             print(err)
             menu_option = ask_again()
@@ -57,7 +57,7 @@ def display_stats(team1, team2, team3):
         1)Panthers \n
         2)Bandits \n
         3)Warriors\n\n''')
-        team_choice = input('Enter an option>>>   ')
+        team_choice = input('Enter an option >>>   ')
         try:
             if int(team_choice) > 3:
                 print('Oops, there are only three teams. Please try again.')
@@ -66,23 +66,27 @@ def display_stats(team1, team2, team3):
                     continue
         if int(team_choice) < 0:
             print("Oops, that wasn't a valid choice, Please try again.")
+
         elif team_choice == '1':
-            names = '{}, {}, {}, {}, {}, {}'.format(team1[0][0]['name'], team1[0][1]['name'], team1[0][2]['name'], team1[0][3]['name'], team1[0][4]['name'], team1[0][5]['name'])
-            print(names)
+            print('\nTeam Name: {}\n -------------------'.format(team_names[0]))
+            print('Total Players: {}\n'.format(len(team1[0])))
+            print('Team Members:\n {}, {}, {}, {}, {}, {}\n'.format(team1[0][0]['name'], team1[0][1]['name'], team1[0][2]['name'], team1[0][3]['name'], team1[0][4]['name'], team1[0][5]['name']))
             menu_option = ask_again()
 
         elif team_choice == '2':
-            names = '{}, {}, {}, {}, {}, {}'.format(team2[0][0]['name'], team2[0][1]['name'], team2[0][2]['name'], team2[0][3]['name'], team2[0][4]['name'], team2[0][5]['name'])
-            print(names)
+            print('\nTeam Name: {}\n -------------------'.format(team_names[1]))
+            print('Total Players: {}\n'.format(len(team2[0])))
+            print('Team Members:\n {}, {}, {}, {}, {}, {}\n'.format(team2[0][0]['name'], team2[0][1]['name'], team2[0][2]['name'], team2[0][3]['name'], team2[0][4]['name'], team2[0][5]['name']))
             menu_option = ask_again()
 
         elif team_choice == '3':
-            names = '{}, {}, {}, {}, {}, {}'.format(team3[0][0]['name'], team3[0][1]['name'], team3[0][2]['name'], team3[0][3]['name'], team3[0][4]['name'], team3[0][5]['name'])
-            print(names)
+            print('\nTeam Name: {}\n -------------------'.format(team_names[2]))
+            print('Total Players: {}\n'.format(len(team3[0])))
+            print('Team Members:\n {}, {}, {}, {}, {}, {}'.format(team3[0][0]['name'], team3[0][1]['name'], team3[0][2]['name'], team3[0][3]['name'], team3[0][4]['name'], team3[0][5]['name']))
             menu_option = ask_again()
 
 
 if __name__ == '__main__':
     player_stats = clean_data(players)
     panthers, bandits, warriors = balance_teams(players, teams)
-    display_stats(panthers, bandits, warriors)
+    display_stats(panthers, bandits, warriors, team_names)
