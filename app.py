2c855d9b37addbe53f024ae68a95cf6b63004a9b
Keith Frazier <keithfrazier98@gmail.com>
2020-07-22 23:15:27 -0700

    Added error handling
    Added error handling to handle out of range options for team selection. Also added a continue option so that it isnt endlessly looping, as well as error handling for the permittable options.

--- app.py
+++ app.py
@@ -36,23 +36,50 @@ def display_stats(team1, team2, team3):
 
     menu_option = input("Enter an option >>>   ")
 
-    while menu_option != 2:
+    def ask_again():
+        menu_option = input("Would you like to (C)ontinue or (Q)uit? >>   ")
+        try:
+            if menu_option == 'c':
+                menu_option = '1'
+            elif menu_option == 'q':
+                menu_option = '2'
+            elif menu_option != 'c' or 'q':
+                raise ValueError("\n\nWhoops, enter 'c' to continue or 'q' to quit.\n\n")
+        except ValueError as err:
+            print(err)
+            menu_option = ask_again()
+        return menu_option
+
+
+    while menu_option != '2':
         print('''
 
         1)Panthers \n
         2)Bandits \n
         3)Warriors\n\n''')
         team_choice = input('Enter an option>>>   ')
-        if team_choice == '1':
+        try:
+            if int(team_choice) > 3:
+                print('Oops, there are only three teams. Please try again.')
+        except ValueError as err:
+                    print('Oops, please enter a valid number. (1, 2, 3)')
+                    continue
+        if int(team_choice) < 0:
+            print("Oops, that wasn't a valid choice, Please try again.")
+        elif team_choice == '1':
             names = '{}, {}, {}, {}, {}, {}'.format(team1[0][0]['name'], team1[0][1]['name'], team1[0][2]['name'], team1[0][3]['name'], team1[0][4]['name'], team1[0][5]['name'])
             print(names)
+            menu_option = ask_again()
+
         elif team_choice == '2':
             names = '{}, {}, {}, {}, {}, {}'.format(team2[0][0]['name'], team2[0][1]['name'], team2[0][2]['name'], team2[0][3]['name'], team2[0][4]['name'], team2[0][5]['name'])
             print(names)
-        else:
+            menu_option = ask_again()
+
+        elif team_choice == '3':
             names = '{}, {}, {}, {}, {}, {}'.format(team3[0][0]['name'], team3[0][1]['name'], team3[0][2]['name'], team3[0][3]['name'], team3[0][4]['name'], team3[0][5]['name'])
             print(names)
-
+            menu_option = ask_again()
 
 
 if __name__ == '__main__':
